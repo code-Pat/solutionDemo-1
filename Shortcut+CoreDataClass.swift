1eb92ce106159f9eb51f8c59c60db0860c024130
Patrick Lee <patlee997@gmail.com>
2021-04-20 21:27:50 +0900

    Core Data [Model] set up
    1. Core Data : [Model] set up
    2. entity: Shortcut
    3. attributes: title, option
    4. Shortcut+CoreDataClass.swift
    5. Shortcut+CoreDataProperties.swift

---
+++ Shortcut+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Shortcut+CoreDataClass.swift
+//  solutionDemo
+//
+//  Created by Patrick Lee on 4/20/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Shortcut)
+public class Shortcut: NSManagedObject {
+
+}
