5fc2bf4b9b27efa626b05fbaccf79d21a15d946c
Patrick Lee <patlee997@gmail.com>
2021-04-20 19:30:45 +0900

    CoreData Model set upt
    *CoreData "Model" set up (manual)
    *Shortcut -> entity
      title, option -> attributes

---
+++ Shortcut+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  Shortcut+CoreDataProperties.swift
+//  solutionDemo
+//
+//  Created by Patrick Lee on 4/20/21.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Shortcut {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Shortcut> {
+        return NSFetchRequest<Shortcut>(entityName: "Shortcut")
+    }
+
+    @NSManaged public var option: String?
+    @NSManaged public var title: String?
+
+}
+
+extension Shortcut : Identifiable {
+
+}
